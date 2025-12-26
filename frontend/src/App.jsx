dfd2ea9bafa26ef73972d1b944b987bc61fa0f36
sagaasachin <sagaasachin48@gmail.com>
2025-12-26 23:49:55 +0530

    Use env-based API and socket URLs (remove localhost)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,9 @@ export default function App() {
     setIsLoggedIn(false);
   }
     console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
+    console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
+    console.log("VITE_SOCKET_URL =", import.meta.env.VITE_SOCKET_URL);
+
 
   return (
     <Container>
